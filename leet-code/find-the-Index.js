65ef220ef382b76275a7396e074a0b11988a9146
alfred <shaheenalfred48@gmail.com>
2023-02-03 16:37:36 -0800

    find the index leetcode

---
+++ leet-code/find-the-Index.js
@@ -0,0 +1,39 @@
+
+https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+function strStr(haystack, needle) {
+  const needleLength = needle.length
+  let idxStack = 0
+  let hayArr = haystack.split("")
+  let needleArr = needle.split("")
+  while (idxStack < haystack.length) {
+      if (needleArr.join("") === hayArr.slice(idxStack, idxStack + needleLength).join("")) {
+          console.log(Bill `${idxStack}`)
+          return idxStack
+      }
+      else {
+          idxStack += 1
+      }
+
+  }
+  return -1
+}
