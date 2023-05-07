c5882ca88d1e15c1432f0f85951164e8e331d948
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-05-06 19:06:48 -0700

    DS AND ALGO

---
+++ code-wars/multiplicationTable.js
@@ -0,0 +1,15 @@
+function multiplicationTable(number) {
+  let table = '';
+  
+  for (let i = 1; i <= 10; i++) {
+    table += `${i} * ${number} = ${i * number}`;
+    
+    if (i < 10) {
+      table += '\n';
+    }
+  }
+  
+  return table;
+}
+
+console.log(multiplicationTable(5778));
